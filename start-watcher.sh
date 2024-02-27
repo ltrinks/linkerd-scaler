63852f02092d2610948c49eeefd56cfaf3127e38
ltrinks <laurentrinks@gmail.com>
2024-02-27 08:03:06 -0500

    refspec issue

--- start-watcher.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl delete -f watcher/watcher.yaml
-kubectl create namespace watcher
-eval $(minikube -p nodevoto docker-env)
-docker build -t watcher watcher
-kubectl apply -f watcher/watcher.yaml
