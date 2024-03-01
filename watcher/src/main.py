cda04c6b4bea3a446eab728139bad0f760ecf175
ltrinks <laurentrinks@gmail.com>
2024-03-01 04:37:20 -0500

    metrics as dict, simple scaler (buggy)

--- watcher/src/main.py
+++ watcher/src/main.py
@@ -1,39 +1,50 @@
 from kubernetes import client, config, watch
-import requests
+
 import glob
 import os
 import time
-from prometheus_client import CollectorRegistry, Gauge, write_to_textfile
+import pprint
+import metrics
+import quantity
+import math
+
 
 # remove previous run
 files = glob.glob('/metrics/*')
 for f in files:
     os.remove(f)
 
-# load the config for the cluster to connect to the API
-config.load_incluster_config()
-
-# clients for namespaces and apps
-coreApiClient = client.CoreV1Api()
-appsApiClient = client.AppsV1Api()
-topApiClient = client.CustomObjectsApi()
-
 f = open("/metrics/pods.txt", "w")
 
-# scale the nodevoto web pod to 2 replicas
-f.write(str(appsApiClient.patch_namespaced_deployment_scale(
-   "web", "nodevoto",
-   {'spec': {'replicas': 2}})) + "\n\n")
+# load the config for the cluster to connect to the API
+config.load_incluster_config()
+appsApiClient = client.AppsV1Api()
 
-# for forever, get CPU and memory of the pods, get the IP addresses of the pods, get prometheus reports
+i = 1
+
+# for forever, get metrics, and adjust scale if needed
 while True:
-    print("Checking pods " + str(time.time()))
-    f.write(str(topApiClient.list_namespaced_custom_object("metrics.k8s.io", "v1beta1", "nodevoto", "pods")) + "\n")
-    for i in coreApiClient.list_namespaced_pod("nodevoto").items:
-        f.write("%s\t%s\t%s\n" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-        pod_file = open("/metrics/%s.txt" % i.metadata.name, "w")
-        pod_file.write(requests.get("http://%s:4191/metrics" % i.status.pod_ip).content.decode())
-        pod_file.close()
-    f.write("\n\n")
+    if (i % 30 == 0):
+        appsApiClient.patch_namespaced_deployment_scale("vote-bot", "nodevoto-bot",{'spec': {'replicas': 1}})
+        f.write("Scaling bot to 1\n")
+
+    elif (i % 10 == 0):
+        appsApiClient.patch_namespaced_deployment_scale("vote-bot", "nodevoto-bot",{'spec': {'replicas': 10}})
+        f.write("Scaling bot to 10\n")
+
+    f.write("Checking pods " + str(time.time()) + "\n")
+    namespace_metrics = metrics.getResourceMetrics("nodevoto")
+    f.write("deployment | cpu | target cpu | count | desired count\n")
+
+    target_cpu = float(quantity.parse_quantity("15m"))
+    for deployment, value in namespace_metrics.items():
+        desired = math.ceil(value["cpu"] / target_cpu)
+        f.write(deployment + " | " + str(value["cpu"]) + " | " + str(target_cpu) + " | " + str(value["count"]) + " | " + str(desired) + " ")
+        if (value["count"] != desired):
+            appsApiClient.patch_namespaced_deployment_scale(deployment, "nodevoto",{'spec': {'replicas': desired}})
+            f.write("SCALING")
+        f.write("\n")
+    f.write("\n")
     f.flush()
-    time.sleep(15)
+    i += 1
+    time.sleep(3)
---
+++ watcher/src/metrics.py
@@ -0,0 +1,63 @@
+from kubernetes import client, config, watch
+import requests
+from prometheus_client import parser
+import quantity
+
+# load the config for the cluster to connect to the API
+config.load_incluster_config()
+
+# clients for namespaces and apps
+coreApiClient = client.CoreV1Api()
+appsApiClient = client.AppsV1Api()
+topApiClient = client.CustomObjectsApi()
+
+# converts watcher-5769cd9645-nsh6d to watcher
+def getPodName(pod):
+    return "".join(pod.split("-")[:-2])
+
+# get the cpu and memory for each deployment in a namespace, also break it down by pod
+# {test: {cpu: 0, memory: 0, count: 1, pods: {test-1234: {cpu: 0, memory: 0}}}}
+def getResourceMetrics(namespace):
+    info = {}
+    for pod in topApiClient.list_namespaced_custom_object("metrics.k8s.io", "v1beta1", namespace, "pods")["items"]:
+        pod_name = getPodName(pod["metadata"]["name"])
+
+        if pod_name not in info:
+            info[pod_name] = {"cpu": 0, "memory": 0, "count": 0, "pods": {}}
+
+        info[pod_name]["count"] += 1
+        info[pod_name]["pods"][pod["metadata"]["name"]] = {"cpu": 0, "memory": 0}
+        for container in pod["containers"]:
+            info[pod_name]["cpu"] += float(quantity.parse_quantity(container["usage"]["cpu"]))
+            info[pod_name]["pods"][pod["metadata"]["name"]]["cpu"] += float(quantity.parse_quantity(container["usage"]["cpu"]))
+            info[pod_name]["memory"] += float(quantity.parse_quantity(container["usage"]["memory"]))
+            info[pod_name]["pods"][pod["metadata"]["name"]]["memory"] += float(quantity.parse_quantity(container["usage"]["memory"]))
+
+    return info
+
+# get resources collected by linkerd injected prometheus
+def getPrometheusMetrics(ip):
+    metrics = list(parser.text_string_to_metric_families(requests.get("http://%s:4191/metrics" % ip).content.decode()))
+    metrics_dict = {}
+    for metric in metrics:
+        metrics_dict[metric.name] = {
+            "type": metric.type, 
+            "unit": metric.unit, 
+            "samples": [
+                {
+                    "labels": sample.labels, 
+                    "value": sample.value, 
+                    "timestamp": sample.timestamp
+                } for sample in metric.samples
+            ]
+        }
+    return metrics_dict
+
+# get resource metrics and add prometheus info for each pod
+def getNamespaceMetrics(namespace):
+    resources = getResourceMetrics(namespace)
+    for i in coreApiClient.list_namespaced_pod(namespace).items:
+        if (i.metadata.name in resources[getPodName(i.metadata.name)]["pods"]):
+            resources[getPodName(i.metadata.name)]["pods"][i.metadata.name]["prometheus"] = getPrometheusMetrics(i.status.pod_ip)
+    return resources
+
