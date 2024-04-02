9ba91d376527849ebdebc4f47f169d3e931ab8a0
ltrinks <laurentrinks@gmail.com>
2024-04-02 18:57:24 -0500

    better cpu reporting

--- watcher/src/calc_responsiveness.py
+++ watcher/src/calc_responsiveness.py
@@ -69,7 +69,7 @@ cpu = [{dep: slice["metrics"][dep]["cpu"] for dep in slice["metrics"]} for slice
 
 def cpu_limit_exceeded(i):
     for dep in cpu[i]:
-        if cpu[i][dep] > quantity.parse_quantity("15m"):
+        if cpu[i][dep] > quantity.parse_quantity("15m") * run[i]["metrics"][dep]["count"]:
             return True
     return False
 
